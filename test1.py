3da604fbaf3aafa3eab1021451b4f7bdfbefb9b6
Daniel <d.sanchez.bassaletti@gmail.com>
2015-09-21 13:46:19 -0300

    ejercicio

---
+++ test1.py
@@ -0,0 +1,16 @@
+prices = {"banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3}
+stock = {"banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15 }
+
+for value in prices:
+    print (value)
+    print ("price: %s" % prices[value])
+    print ("stock: %s" % stock[value])
+    
+
+    
